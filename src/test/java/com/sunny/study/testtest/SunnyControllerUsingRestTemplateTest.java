7c81f15bb71f89bf3480e4bed76a87fc65f4a364
Taesun Lee <superbsun@gmail.com>
2020-12-13 17:51:49 +0900

    test WebEnvironment.RANDOM_PORT tester

---
+++ src/test/java/com/sunny/study/testtest/SunnyControllerUsingRestTemplateTest.java
@@ -0,0 +1,26 @@
+package com.sunny.study.testtest;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class SunnyControllerUsingRestTemplateTest {
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Test
+    public void hello() throws Exception {
+        String result = testRestTemplate.getForObject("/hello", String.class);
+        assertThat(result).isEqualTo("hello sunny");
+    }
+}
