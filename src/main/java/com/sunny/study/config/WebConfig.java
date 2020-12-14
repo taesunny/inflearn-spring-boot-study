4b9a8d3e6c84995afcb583a35f5e19605b7e4031
Taesun Lee <superbsun@gmail.com>
2020-12-14 22:36:04 +0900

    index.html page test

---
+++ src/main/java/com/sunny/study/config/WebConfig.java
@@ -0,0 +1,10 @@
+package com.sunny.study.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+}
