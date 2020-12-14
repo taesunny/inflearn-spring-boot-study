4bdf6e65e1f461211146d445a2648f4a776c7a37
Taesun Lee <superbsun@gmail.com>
2020-12-14 21:54:56 +0900

    web mvc post test - xml result test

--- src/test/java/com/sunny/study/user/UserControllerTest.java
+++ src/test/java/com/sunny/study/user/UserControllerTest.java
@@ -40,4 +40,17 @@ public class UserControllerTest {
                 .andExpect(jsonPath("$.username", is(equalTo("sunny"))))
                 .andExpect(jsonPath("$.password", is(equalTo("123"))));
     }
+
+    @Test
+    public void createUser_XML() throws Exception {
+        String userJson = "{\"username\":\"sunny\", \"password\": \"123\"}";
+
+        mockMvc.perform(post("/users/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_XML)
+                .content(userJson))
+                .andExpect(status().isOk())
+                .andExpect(xpath("/User/username").string("sunny"))
+                .andExpect(xpath("/User/password").string("123"));
+    }
 }
